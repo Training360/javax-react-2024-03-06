3ad047f737df4f17ccf31db3c2b1bd932c12dbbd
vicziani <viczian.istvan@gmail.com>
2024-03-06 11:30:58 +0100

    Lombok bevezetése, első entitás

---
+++ employees-se/src/main/java/employees/Employee.java
@@ -0,0 +1,13 @@
+package employees;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+
+    private Long id;
+
+    private String name;
+}
