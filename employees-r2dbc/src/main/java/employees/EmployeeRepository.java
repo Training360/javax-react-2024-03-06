4053495169f6fa547e5d4c19c398bd1415f8ad88
vicziani <viczian.istvan@gmail.com>
2024-03-07 11:41:02 +0100

    Adatbáziskezelés kiindulási állapot

---
+++ employees-r2dbc/src/main/java/employees/EmployeeRepository.java
@@ -0,0 +1,49 @@
+package employees;
+
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class EmployeeRepository {
+
+    private AtomicLong sequenceGenerator = new AtomicLong();
+
+    private final List<Employee> employees = Collections.synchronizedList(new ArrayList<>(List.of(
+            new Employee(sequenceGenerator.incrementAndGet(), "John Doe"),
+            new Employee(sequenceGenerator.incrementAndGet(), "Jack Doe")
+    )));
+
+    public Flux<Employee> findAll() {
+        return Flux.fromIterable(employees);
+    }
+
+    public Mono<Employee> findById(long id) {
+        return Flux.fromIterable(employees)
+                .filter(e -> e.getId() == id)
+                .singleOrEmpty();
+    }
+
+    public Mono<Employee> save(Employee employee) {
+        if (employee.getId() == null) {
+            return Mono.just(employee)
+                    .doOnNext(e -> e.setId(sequenceGenerator.incrementAndGet())) // Nincs mellékhatás
+                    .doOnNext(employees::add); // Van mellékhatás, kerüljük
+        }
+        else {
+//            return findById(employee.getId())
+//                    .doOnNext(e -> e.setName(employee.getName()));
+            return Mono.just(employee);
+        }
+    }
+
+    public Mono<Void> deleteById(long id) {
+        employees.removeIf(e -> e.getId() == id);
+        return Mono.empty();
+    }
+}
