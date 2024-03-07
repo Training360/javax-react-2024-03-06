73798381a26f400b317639f38926053e5160c7e2
vicziani <viczian.istvan@gmail.com>
2024-03-07 15:42:39 +0100

    Kliens kiinduló projekt

---
+++ employees-client/src/main/java/training/employeesclient/EmployeesClientApplication.java
@@ -0,0 +1,13 @@
+package training.employeesclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesClientApplication.class, args);
+	}
+
+}
---
+++ employees-client/src/test/java/training/employeesclient/EmployeesClientApplicationTests.java
@@ -0,0 +1,13 @@
+package training.employeesclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
