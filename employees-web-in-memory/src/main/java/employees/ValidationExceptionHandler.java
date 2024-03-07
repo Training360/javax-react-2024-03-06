e2d860b63f2b2a63656b03d22b3ad4c8678bed10
vicziani <viczian.istvan@gmail.com>
2024-03-07 09:54:11 +0100

    validation

--- employees-web-in-memory/src/main/java/employees/ValidationExceptionHandler.java
+++ employees-web-in-memory/src/main/java/employees/ValidationExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
@@ -16,12 +17,28 @@ public class ValidationExceptionHandler {
     public Mono<ProblemDetail> handle(WebExchangeBindException exception) {
         return Mono.just(exception)
                 .map(e -> {
-                    var problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+                    // e.getMessage()
+                    var problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Validation error");
                     problem.setType(URI.create("employees/validation-error"));
                     problem.setTitle("Validation error");
+
+//                    var messages = Flux.fromIterable(e.getBindingResult().getFieldErrors())
+//                            .map(fieldError -> new FieldMessage(fieldError.getField(), fieldError.getDefaultMessage())).collectList().block();
+
+                    var messages = e.getBindingResult()
+                            .getFieldErrors()
+                            .stream()
+                            .map(fieldError -> new FieldMessage(fieldError.getField(), fieldError.getDefaultMessage()))
+                                    .toList();
+
+                    problem.setProperty("messages", messages);
 
                     return problem;
                         }
                         );
     }
+
+
+
+    // TODO JSON syntax error
 }
