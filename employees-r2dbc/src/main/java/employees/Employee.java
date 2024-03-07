d292d2c157b30a8855450315a4a40586088f028f
vicziani <viczian.istvan@gmail.com>
2024-03-07 12:00:04 +0100

    Adatbáziskezelés R2DBC-vel

--- employees-r2dbc/src/main/java/employees/Employee.java
+++ employees-r2dbc/src/main/java/employees/Employee.java
@@ -3,12 +3,14 @@ package employees;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
 
+    @Id
     private Long id;
 
     private String name;
---
+++ employees-r2dbc/src/main/resources/db/migration/V01__employees.sql
@@ -0,0 +1,1 @@
+create table employee(id serial primary key, name varchar(255));
